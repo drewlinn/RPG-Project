7caae65bf9223e413384deeb0bbb4db0be7f8aaf
Andrew Dalton and Jordan Mysliwiec <andrew+jordan@>
2017-08-09 10:44:03 -0700

    successfully migrate our database.

---
+++ src/RPG/Migrations/20170809173633_Initial.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using RPG.Models;
+
+namespace RPG.Migrations
+{
+    [DbContext(typeof(RPGContext))]
+    [Migration("20170809173633_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("RPG.Models.Player", b =>
+                {
+                    b.Property<int>("id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("image");
+
+                    b.Property<int>("inventoryId");
+
+                    b.Property<string>("name");
+
+                    b.HasKey("id");
+
+                    b.ToTable("Players");
+                });
+        }
+    }
+}
