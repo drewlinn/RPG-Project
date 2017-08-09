2548d6d4c07662b31f6befdb2024ff8dbc2b9906
Andrew Dalton and Jordan Mysliwiec <andrew+jordan@>
2017-08-09 14:27:24 -0700

    successfully build and run some basic tests.

---
+++ src/RPG/Controllers/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RPG.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RPG.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private RPGContext db = new RPGContext();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+           return View(db.Players.ToList());
+        }
+    }
+}
