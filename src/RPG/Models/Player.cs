c9f7d331f2bda258684b7df66e883eb19537e3be
Andrew Dalton and Jordan Mysliwiec <andrew+jordan@>
2017-08-09 09:30:32 -0700

    initial commit.

---
+++ src/RPG/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPG.Models
+{
+    [Table("Players")]
+    public class Player
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public int inventoryId { get; set;  }
+    }
+}
---
+++ src/RPG/Models/RPGContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RPG.Models
+{
+    public class RPGContext : DbContext
+    {
+        public RPGContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        public RPGContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Player> Players { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=RPG;integrated security=True");
+        }
+    }
+}
