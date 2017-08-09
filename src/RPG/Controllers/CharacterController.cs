b47da74109118cb7e1c789d67549b098aa6c5c9b
Andrew Dalton and Jordan Mysliwiec <andrew+jordan@>
2017-08-09 16:00:17 -0700

    successfully build fill CRUD functionality for our Character object.

--- src/RPG/Controllers/CharacterController.cs
+++ src/RPG/Controllers/CharacterController.cs
@@ -1,21 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RPG.Models;
+using System;
 
-namespace RPG.Controllers
+
+namespace RP.Controllers
 {
     public class CharacterController : Controller
     {
         private RPGContext db = new RPGContext();
-        // GET: /<controller>/
         public IActionResult Index()
         {
-           return View(db.Characters.ToList());
+            return View(db.Characters.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisCharacter = db.Characters.FirstOrDefault(cha => cha.id == id);
+
+            return View(thisCharacter);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Character character)
+        {
+            db.Characters.Add(character);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var thisCha = db.Characters.FirstOrDefault(cha => cha.id == id);
+            return View(thisCha);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisCha = db.Characters.FirstOrDefault(cha => cha.id == id);
+            db.Characters.Remove(thisCha);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisCha = db.Characters.FirstOrDefault(cha => cha.id == id);
+            return View(thisCha);
+        }
+        [HttpPost]
+        public IActionResult Edit(Character character)
+        {
+            db.Entry(character).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Error()
+        {
+            return View();
         }
     }
 }
